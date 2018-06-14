fe8fb106cea42f7be6b7ab64c2a5a4cf7b9babde
Sergey Lozenkov <slozenkov@gmail.com>
2018-06-15 02:09:50 +0400

    packer config

---
+++ packer/scripts/all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# install ruby
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm requirements
+rvm install 2.4.1
+rvm use 2.4.1 --default
+gem install bundler -V --no-ri --no-rdoc
+
+# install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+# deploy app
+pwd
+git clone https://github.com/Artemmkin/reddit.git
+cd reddit
+pwd
+bundle install
+puma -d
+
---
+++ packer/scripts/install_ruby.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm requirements
+rvm install 2.4.1
+rvm use 2.4.1 --default
+gem install bundler -V --no-ri --no-rdoc
+
